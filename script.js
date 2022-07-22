c2c8cc8d45b733858be893df3b437e359aadf042
Lukas Koeller <acc.koeller@protonmail.com>
2022-07-22 11:21:39 +0200

    chore: add categories to logins

--- script.js
+++ script.js
@@ -2,12 +2,20 @@ const logins = [
     {
         username: "owner5@mlvv.de",
         password: "Berlin.123",
-        description: "Einzel, 2 BPs, mit Geld"
+        description: "Einzel, 2 BPs, mit Geld",
+        categories: {
+            entrypointId: 'NEW_CUSTOMER__NEW_BP__TANGIBLE_ASSETS',
+            accountType: 'childSingleAccount',
+        }
     },
     {
         username: "sebastian-test@mailinator.com",
         password: "Berlin.1234567",
-        description: "Einzel, viele ThemenVVs, PREMIUM"
+        description: "Einzel, viele ThemenVVs, PREMIUM",
+        categories: {
+            entrypointId: 'NEW_CUSTOMER__NEW_BP__VV',
+            accountType: 'singleAccount',
+        }
     }
 ];
 
